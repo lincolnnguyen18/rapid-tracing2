8ad43807b04ee9dc4d2137dd219ef7ad38ba084c
lincolnnguyen18 <letsmakeaworld@icloud.com>
2022-02-21 06:20:54 -0600

    get time

--- init.sql
+++ init.sql
@@ -88,4 +88,11 @@ CREATE PROCEDURE get_user_picture_time_records(_user_id INTEGER, _picture_id INT
   ORDER BY time_records.id ASC;
 END//
 
+CREATE PROCEDURE get_user_total_time_records_for_date(_user_id INTEGER, _date DATE) BEGIN
+  SELECT SUM(seconds) / 60 as total_minutes FROM time_records
+  JOIN picture_timerecords ON time_records.id = picture_timerecords.timerecord_id
+  JOIN user_pictures ON picture_timerecords.picture_id = user_pictures.picture_id
+  WHERE user_pictures.user_id = _user_id AND time_records.created_at >= _date AND time_records.created_at < _date + INTERVAL 1 DAY;
+END//
+
 DELIMITER ;
